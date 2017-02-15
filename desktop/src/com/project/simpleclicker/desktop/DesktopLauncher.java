ef769e4117df92cd6dffeb0689c66e993c9b6bf3
VicMr06 <victor.chukwu06@gmail.com>
2017-02-15 12:00:09 +0100

    init

---
+++ desktop/src/com/project/simpleclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.project.simpleclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.project.simpleclicker.SimpleClicker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SimpleClicker(), config);
+	}
+}
