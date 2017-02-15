b3478b5ad2e84b9d5af165538f99d9ebf1425335
VicMr06 <victor.chukwu06@gmail.com>
2017-02-15 17:19:36 +0100

    added Gameplay Screen

---
+++ core/src/com/project/simpleclicker/screens/GameplayScreen.java
@@ -0,0 +1,16 @@
+package com.project.simpleclicker.screens;
+
+import com.project.simpleclicker.SimpleClicker;
+
+public class GameplayScreen extends AbstractScreen {
+	
+	public GameplayScreen(SimpleClicker game){
+		super(game);
+		init();
+	}
+
+	private void init() {
+		
+	}
+		
+}
