16960e49a10d667b0b9f8bf7574a490d60d7a457
VicMr06 <victor.chukwu06@gmail.com>
2017-02-17 16:58:14 +0100

    refactor

--- core/src/com/project/simpleclicker/SimpleClicker.java
+++ core/src/com/project/simpleclicker/SimpleClicker.java
@@ -1,25 +1,26 @@
 package com.project.simpleclicker;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.project.simpleclicker.screens.SplashScreen;
 
 public class SimpleClicker extends Game{
 	
 	public final static String GAME_NAME = "Simple Clicker";
-	//public final static String GAME_NAME = "";
-	
+		
 	public static int WIDTH= 480;
 	public static int HEIGHT= 700;
 	
 	private boolean paused;
 	
+	private int points;
+	
 	@Override
 	public void create () {
 		this.setScreen(new SplashScreen(this));
+	}
+	
+	public void addPoints(){
+		points++;
 	}
 
 
@@ -36,4 +37,12 @@ public class SimpleClicker extends Game{
 	public void setPaused(boolean paused) {
 		this.paused = paused;
 	}
+
+
+	public int getPoints() {
+		return points;
+	}
+
+
+	
 }
