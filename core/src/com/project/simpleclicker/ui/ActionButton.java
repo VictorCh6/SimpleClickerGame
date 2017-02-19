f1cc57eff24711a24a8e121239981a129313f375
VicMr06 <victor.chukwu06@gmail.com>
2017-02-19 16:07:22 +0100

    added action buttons skins

--- core/src/com/project/simpleclicker/ui/ActionButton.java
+++ core/src/com/project/simpleclicker/ui/ActionButton.java
@@ -1,23 +1,37 @@
 package com.project.simpleclicker.ui;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class ActionButton extends Button {
 
 	public ActionButton(final IClickCallback callback, int x){
-		super(new ButtonStyle());
+		super(prepareButtonStyle());
 		init(callback, x);		
 	}
 
 	
+
+	private static ButtonStyle prepareButtonStyle(){
+			TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("ui-green.atlas"));
+			Skin skin = new Skin(atlas);
+			ButtonStyle buttonStyle = new ButtonStyle();
+			buttonStyle.up = skin.getDrawable("button_05");
+			buttonStyle.down = skin.getDrawable("button_06");
+			
+			return buttonStyle;
+	}
+
+
 	private void init(final IClickCallback callback,int x) {
 		this.setWidth(140);
 		this.setHeight(50);
 		this.setX(x);
 		this.setY(110);
-		this.setDebug(true);
 		
 		this.addListener(new ClickListener(){
 			
