abc5a1a5dbaf5ba51f1b74f2cd0c9d85d3f33b4c
VicMr06 <victor.chukwu06@gmail.com>
2017-02-19 12:23:21 +0100

    added random actions to class FlyingLivestock

--- core/src/com/project/simpleclicker/entities/FlyingLivestock.java
+++ core/src/com/project/simpleclicker/entities/FlyingLivestock.java
@@ -1,6 +1,7 @@
 package com.project.simpleclicker.entities;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
@@ -25,8 +26,12 @@ public class FlyingLivestock extends Image {
 	private final static int WIDHT = 150;
 	private final static int HEIGHT = 150;
 	
-	private final static int STARTING_X = 0;
-	private final static int STARTING_Y = -100;
+	private final static int STARTING_X_1 = 0;
+	private final static int STARTING_X_2 = SimpleClicker.WIDTH;
+	private final static int STARTING_Y_1 = -100;
+	private final static int STARTING_Y_2 = SimpleClicker.HEIGHT;
+	private int startingX;
+	private int startingY;
 	
 	private SimpleClicker game;
 	private FlyingLivestockType type;
@@ -41,7 +46,9 @@ public class FlyingLivestock extends Image {
 		this.setSize(WIDHT, HEIGHT);
 		
 		//Starting position
-		this.setPosition(STARTING_X, STARTING_Y);
+		startingX = MathUtils.randomBoolean() ? STARTING_X_1 : STARTING_X_2;
+		startingY = MathUtils.randomBoolean() ? STARTING_Y_1 : STARTING_Y_2;
+		this.setPosition(startingX, startingY);
 		
 		this.addListener(new ClickListener(){
 			@Override
@@ -60,18 +67,19 @@ public class FlyingLivestock extends Image {
 		if(FlyingLivestockType.PIG.equals(type)){
 			game.addPointsExtra(50); System.out.println("PIG");
 		} else if (FlyingLivestockType.COW.equals(type)){
-			System.out.println("COW");
+			game.addPointsExtra(100); System.out.println("COW");
 		} else if(FlyingLivestockType.SHEEP.equals(type)){
-			System.out.println("SHEEP");
+			game.addPointsExtra(40); System.out.println("SHEEP");
 		} else if(FlyingLivestockType.FOX.equals(type)){
-			System.out.println("FOX");
+			game.addPointsExtra(-100); System.out.println("FOX");
 		} else if(FlyingLivestockType.BADCAT.equals(type)){
-		 	System.out.println("BADCAT");
+			game.addPointsExtra(-20); System.out.println("BADCAT");
 		} else if(FlyingLivestockType.RABBIT.equals(type)){
-			System.out.println("RABBIT");
+			game.addPointsExtra(20); System.out.println("RABBIT");
 		} else if(FlyingLivestockType.CRAZYPIG.equals(type)){
-			System.out.println("CRAZYPIG");
+			game.addPointsExtra(-50); System.out.println("CRAZYPIG");
 		}
+		
 		FlyingLivestock.this.remove();
 		
 	}
@@ -93,20 +101,33 @@ public class FlyingLivestock extends Image {
 			} else if (FlyingLivestockType.BADCAT.equals(type)) {
 				return BADCAT;
 			} 
-				return"";
+			
+			return"";
 		}	
 
 		
 		public void fly(){
 			
+			int xSign = 0;
+			if(startingX == STARTING_X_1){
+				xSign = 1;
+			} else {
+				xSign = -1;
+			}
+			
+			int time1 = MathUtils.random(1, 6);
+			int time2 = MathUtils.random(1, 6);
+			
+			int randomYEffect = MathUtils.random(-100, 500);
+			
 			Action a = Actions.parallel(
-					Actions.moveBy(300, 200, 5),
-					Actions.rotateBy(360, 5)
+					Actions.moveBy(xSign * 300 + (MathUtils.random(-200, 200)), 200 + randomYEffect, time1),
+					Actions.rotateBy(360, time1)
 					);
 			
 			Action b = Actions.parallel(
-					Actions.moveBy(-500, 900, 3),
-					Actions.rotateBy(360, 3)
+					Actions.moveBy(xSign * -500 + (MathUtils.random(-200, 200)), 900, time2),
+					Actions.rotateBy(360, time2)
 					);
 			
 			Action c = Actions.run(new Runnable() {
